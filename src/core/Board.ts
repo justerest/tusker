06fb2f84a0ac27aab22d28285f1f1a6c2d667ed4
justerest <justerest@gmailcom>
2020-02-02 22:28:20 +0500

    feat: init project, board

---
+++ src/core/Board.ts
@@ -0,0 +1,60 @@
+import { Employee } from './employee/Employee';
+import { Task } from './task/Task';
+import { assert } from 'src/utils/assert';
+import { Time } from './task/Time';
+import { WorkingTime } from './employee/WorkingTime';
+
+export class Board {
+  private employeeSet: Set<Employee['id']> = new Set();
+  private taskSet: Set<Task['id']> = new Set();
+  private completed: boolean = false;
+
+  date: Date;
+
+  constructor(date: Date) {
+    this.date = date;
+  }
+
+  isCompleted(): boolean {
+    return this.completed;
+  }
+
+  markAsCompleted(): void {
+    this.completed = true;
+  }
+
+  getTaskIds(): Task['id'][] {
+    return [...this.taskSet.values()];
+  }
+
+  getEmployeeIds(): Employee['id'][] {
+    return [...this.employeeSet.values()];
+  }
+
+  planeTask(title: string, plannedTime: Time): Task {
+    assert(!this.isCompleted());
+    const task = new Task();
+    task.title = title;
+    task.plannedTime = plannedTime;
+    this.taskSet.add(task.id);
+    return task;
+  }
+
+  addEmployee(name: string, workingTime: WorkingTime): Employee {
+    assert(!this.isCompleted());
+    const employee = new Employee();
+    employee.name = name;
+    employee.workingTime = workingTime;
+    this.employeeSet.add(employee.id);
+    return employee;
+  }
+
+  createNextBoard(employeeIds: Employee['id'][] = [], taskIds: Task['id'][] = []): Board {
+    const date = new Date(this.date);
+    date.setDate(date.getDate());
+    const board = new Board(date);
+    board.employeeSet = new Set(employeeIds);
+    board.taskSet = new Set(taskIds);
+    return board;
+  }
+}
---
+++ src/core/Project.ts
@@ -0,0 +1,45 @@
+import { Identity } from './common/Identity';
+import { Board } from './Board';
+import { assert } from 'src/utils/assert';
+import { Employee } from './employee/Employee';
+import { last } from 'lodash';
+
+export class Project {
+  private boardList: Board[] = [];
+  private employeeSet: Set<Employee['id']> = new Set();
+
+  id: Identity = Identity.random();
+
+  constructor() {}
+
+  getBoards(): Board[] {
+    return this.boardList.slice();
+  }
+
+  getActiveBoard(): Board {
+    const activeBoard = this.boardList.find((board) => !board.isCompleted());
+    assert(activeBoard, 'Active board not found');
+    return activeBoard;
+  }
+
+  geEmployeeIds(): Employee['id'][] {
+    return [...this.employeeSet.values()];
+  }
+
+  planeNextBoard(date?: Date): Board {
+    const activeBoard = this.getActiveBoard();
+    assert(activeBoard === last(this.boardList), 'Can plane only one board after active');
+    const board = activeBoard.createNextBoard(this.geEmployeeIds());
+    if (date) {
+      assert(date > activeBoard.date, 'Can not plane board for past');
+      board.date = date;
+    }
+    this.boardList.push(board);
+    return board;
+  }
+
+  addEmployee(employeeId: Employee['id']): void {
+    assert(!this.employeeSet.has(employeeId), 'Employee already exist');
+    this.employeeSet.add(employeeId);
+  }
+}
